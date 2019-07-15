32eea38af894a1e8e2f204a43b8f803dd16d0ca5
sharpmax <maxim.sulkouski@gmail.com>
2019-07-16 01:53:30 +0200

    Added bit shift left functionality, fixed bug where integers which are not equal to factor of 2 have been wrongly converted to binary

--- BinaryConverter/src/com/company/BinaryConverter.java
+++ BinaryConverter/src/com/company/BinaryConverter.java
@@ -1,52 +1,130 @@
 package com.company;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 class BinaryConverter {
     private int decimalValue;
     private boolean[] binaryNumber = new boolean[32];
+    private boolean[] onesComplement = new boolean[32];
+    private boolean[] twosComplement = new boolean[32];
+    private boolean isNegative;
 
     public BinaryConverter(int decimalValue) {
         this.decimalValue = decimalValue;
+    }
+
+    public BinaryConverter() {
+        this.decimalValue = 0;
     }
 
     public void setDecimalValue(int decimalValue) {
         this.decimalValue = decimalValue;
     }
 
-    public String convert() {
+    public String getBinaryNumber() {
+        return convertToString(binaryNumber);
+    }
+
+    public String getOnesComplement() {
+        return convertToString(onesComplement);
+    }
+
+    public String getTwosComplement() {
+        return convertToString(twosComplement);
+    }
+
+    public boolean isNegative() {
+        return isNegative;
+    }
+
+    public void shiftLeft(int distance) {
+        int index =  0;
+        boolean[] temp = new boolean[binaryNumber.length - 1 - distance];
+
+        for (int i = 0; i < temp.length; i++) {
+            temp[i] = binaryNumber[i];
+        }
+
+        for (int i = distance; i < binaryNumber.length; i++) {
+            binaryNumber[i] = temp[index];
+            if (index < temp.length - 1) {
+                index++;
+            }
+        }
+
+        for (int i = 0; i < distance; i++) {
+            binaryNumber[i] = false;
+        }
+    }
+
+    public void shiftRight(int distance) {
+
+        boolean[] shift = new boolean[distance];
+
+
+    }
+
+
+    public void convert() {
         int candidate = this.decimalValue;
         int bitLocation;
 
 
         if (candidate < 0) {
             candidate = Math.abs(candidate);
-            binaryNumber[31] = true;
+            this.isNegative = true;
         }
         while (candidate > 0) {
             if (candidate == 1) {
                 this.binaryNumber[0] = true;
                 break;
-            }
+                }
             bitLocation = 1;
             while (Math.pow(2,bitLocation) < candidate) {
-                if (Math.pow(2, bitLocation+1) > candidate)
+                if (Math.pow(2, (bitLocation+1)) > candidate)
                     break;
                 bitLocation++;
-            }
-            if (bitLocation == 32) {
-                return "Sorry, the number is greater than allowed by int";
-            }
+                }
+
             if (bitLocation > 0) {
                 this.binaryNumber[bitLocation] = true;
                 candidate -= Math.pow(2, bitLocation);
+                }
+
             }
-        }
-        return convertToString();
+
+            if (this.isNegative) {
+                convertToNegative(binaryNumber);
+            }
+
     }
 
-    private String convertToString() {
+    private void convertToNegative(boolean[] binaryNumber) {
+        this.binaryNumber[31] = true;
+        this.onesComplement = Arrays.copyOf(binaryNumber,32);
+        System.out.println(convertToString(this.onesComplement));
+
+        for (int i = 0; i < onesComplement.length - 1; i++) {
+            this.onesComplement[i] = !this.onesComplement[i];
+        }
+
+        this.twosComplement = Arrays.copyOf(this.onesComplement,32);
+        for (int i = 0; i < this.twosComplement.length - 1; i++) {
+            if (this.twosComplement[i]) {
+                this.twosComplement[i] = false;
+            }
+            else {
+                this.twosComplement[i] = true;
+                break;
+            }
+        }
+    }
+
+    private String convertToString(boolean[] value) {
         char[] ch = new char[32];
         for (int i = 31; i >= 0; i--) {
-            if (this.binaryNumber[Math.abs(i - 31)]) {
+            if (value[Math.abs(i - 31)]) {
                 ch [i] = '1';
             }
             else {
@@ -56,5 +134,4 @@ class BinaryConverter {
         }
         return String.valueOf(ch);
     }
-
 }
--- BinaryConverter/src/com/company/Main.java
+++ BinaryConverter/src/com/company/Main.java
@@ -3,15 +3,70 @@ package com.company;
 import java.util.Scanner;
 
 class Main {
-    static final Scanner input = new Scanner(System.in);
+    private static final Scanner input = new Scanner(System.in);
+    private static boolean quit = false;
+    private static BinaryConverter converter = new BinaryConverter();
 
     public static void main(String[] args) {
-        boolean quit = false;
+        boolean valueAvailable = false;
         int value;
 
+
         do {
-            System.out.println("Welcome to decimal to binary converter.");
-            System.out.println("Please enter value to be converted (Q to quit): ");
+            if (valueAvailable) {
+                displayBitShift();
+                try {
+                    value = Integer.parseInt(input.nextLine());
+                }
+                catch (NumberFormatException e) {
+                    quit = true;
+                    continue;
+                }
+
+
+                if(value == 1) {
+
+                    System.out.println("Enter shift distance (max 30: ");
+                    try {
+                        value = Integer.parseInt(input.nextLine());
+                    }
+                    catch (NumberFormatException e) {
+                        quit = true;
+                        continue;
+                    }
+                    if (value >=0 & value < 31) {
+                        converter.shiftLeft(value);
+                        value = 0;
+                    }
+                    else {
+                        quit = true;
+                        continue;
+                    }
+
+                }
+
+                if (value == 2) {
+                    System.out.println("Enter shift distance (max 30: ");
+                    try {
+                        value = Integer.parseInt(input.nextLine());
+                    }
+                    catch (NumberFormatException e) {
+                        quit = true;
+                        continue;
+                    }
+                    if (value >=0 & value < 31) {
+                        converter.shiftRight(value);
+                    }
+                    else {
+                        quit = true;
+                        continue;
+                    }
+                }
+
+                displayBinary();
+            }
+            displayMainMenu();
+
             try {
                 value = Integer.parseInt(input.nextLine());
             }
@@ -19,8 +74,55 @@ class Main {
                 quit = true;
                 continue;
             }
-            BinaryConverter converter = new BinaryConverter(value);
-            System.out.println(converter.convert());
+
+            if (value != 1) {
+                quit = true;
+                continue;
+            }
+            else {
+                System.out.println("Please enter value: ");
+                valueAvailable = true;
+            }
+
+            try {
+                value = Integer.parseInt(input.nextLine());
+            }
+            catch (NumberFormatException e) {
+                quit = true;
+                continue;
+            }
+
+            converter.setDecimalValue(value);
+            converter.convert();
+
+            displayBinary();
+
         } while (!quit);
+    }
+
+    private static void displayBinary() {
+        if (converter.isNegative()) {
+            System.out.println("Binary value: " + converter.getBinaryNumber());
+            System.out.println("One's complement" + converter.getOnesComplement());
+            System.out.println("Two's complement" + converter.getTwosComplement());
+        }
+        else {
+            System.out.println("Binary value: " + converter.getBinaryNumber());
+        }
+    }
+
+    private static void displayMainMenu() {
+        System.out.println("Welcome to the Bit Converter!");
+        System.out.println("\n\nMain Menu:\n1) Enter decimal value\n");
+    }
+
+    private static void displayBinaryMenu() {
+        System.out.println("1) Shift bits left\n\n2) Shift bits right");
+    }
+
+    private static void displayBitShift() {
+        System.out.println("1) shift left\n2) shift right:\n");
+
+
     }
 }
